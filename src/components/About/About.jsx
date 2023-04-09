02aea2e0656e3350e45bcbe29b9a26970f7a1bb8
ObodoakorDavid <obodoakordavid@outlook.com>
2023-04-09 19:02:47 +0100

    updated dir

---
+++ src/components/About/About.jsx
@@ -0,0 +1,106 @@
+/** @format */
+
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>
+        ABOUT <span>ME</span>
+      </h1>
+      <div className="information">
+        <div className="inner-information1">
+          <p>
+            I am a budding FullStack Web Developer, currently navigating and
+            learning the web one line of code at a time, eagerly willing to
+            learn anything that has to do with building and making the web look
+            good and also function optimally.
+          </p>
+          <p>
+            I am a free-spirited person who enjoys inddor activities like
+            table-tennis, board games, video games and watching TV Shows/Movies.
+            Some outdoor activites are sometimes fancied but inddor activites
+            are preferred.
+          </p>
+        </div>
+        <div className="inner-information2">
+          <div>
+            <div>
+              <div>
+                <p>20+</p>
+              </div>
+              <p>Projects Completed</p>
+            </div>
+            <div>
+              <div>
+                <p>1+</p>
+              </div>
+              <p>Years Of Experience</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h2>MY SKILLS</h2>
+      <div className="allprogress">
+        <div className="progress">
+          <p>HTML5</p>
+          <div>
+            <p>90%</p>
+            <div className="progress-inner">
+              <div className="progressbar html"></div>
+            </div>
+          </div>
+        </div>
+        <div className="progress">
+          <p>CSS</p>
+          <div>
+            <p>80%</p>
+            <div className="progress-inner">
+              <div className="progressbar css"></div>
+            </div>
+          </div>
+        </div>
+        <div className="progress">
+          <p>JavaScript</p>
+          <div>
+            <p>70%</p>
+            <div className="progress-inner">
+              <div className="progressbar javascript"></div>
+            </div>
+          </div>
+        </div>
+        <div className="progress">
+          <p>ReactJs</p>
+          <div>
+            <p>70%</p>
+            <div className="progress-inner">
+              <div className="progressbar react"></div>
+            </div>
+          </div>
+        </div>
+        <div className="progress">
+          <p>Python</p>
+          <div>
+            <p>40%</p>
+            <div className="progress-inner">
+              <div className="progressbar python"></div>
+            </div>
+          </div>
+        </div>
+        <div className="progress">
+          <p>Django</p>
+          <div>
+            <p>30%</p>
+            <div className="progress-inner">
+              <div className="progressbar django"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Navbar/Navbarr.jsx
@@ -0,0 +1,88 @@
+/** @format */
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbarr.css";
+
+import { icons } from "../../assets/icons";
+
+console.log("hh");
+
+const Navbarr = () => {
+  const [image, setImage] = useState(icons.brightness);
+  const [active, setActive] = useState("home");
+  // console.log(
+  //   document.querySelector("meta[name=theme-color]").getAttribute("content")
+  // );
+  document
+    .querySelector("meta[name=theme-color]")
+    .setAttribute("content", "#191d2b");
+
+  function lightmode() {
+    let body = document.body;
+    body.classList.toggle("lightmode");
+    if (body.classList.contains("lightmode")) {
+      setImage(icons.moon);
+      document
+        .querySelector("meta[name=theme-color]")
+        .setAttribute("content", "#ffffff");
+    } else {
+      setImage(icons.brightness);
+      document
+        .querySelector("meta[name=theme-color]")
+        .setAttribute("content", "#191d2b");
+    }
+  }
+  return (
+    <header>
+      <nav className="nav-menu">
+        <Link to="/">
+          <img
+            className={active === "home" ? "home icons active" : "home icons"}
+            onClick={() => setActive("home")}
+            src={icons.Home}
+            alt=""
+          />
+        </Link>
+        <Link to="/aboutme">
+          <img
+            className={
+              active === "aboutMe" ? "aboutMe icons active" : "aboutMe icons"
+            }
+            onClick={() => setActive("aboutMe")}
+            src={icons.AboutMe}
+            alt=""
+          />
+        </Link>
+        <Link to="/portfolio">
+          <img
+            className={
+              active === "portfolio-nav"
+                ? "portfolio-nav icons active"
+                : "portfolio-nav icons"
+            }
+            onClick={() => setActive("portfolio-nav")}
+            src={icons.Portfolio}
+            alt=""
+          />
+        </Link>
+
+        <Link to="/contactme">
+          <img
+            className={
+              active === "contactMe"
+                ? "contactMe icons active"
+                : "contactMe icons"
+            }
+            onClick={() => setActive("contactMe")}
+            src={icons.ContactMe}
+            alt=""
+          />
+        </Link>
+        <img className="darkk" onClick={lightmode} src={image} alt="" />
+      </nav>
+    </header>
+  );
+};
+
+export default Navbarr;
